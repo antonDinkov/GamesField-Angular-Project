1ce828ffe0f4da588b2052c4a176601d9e9f6070
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-12 09:21:49 +0300

    home into components

--- GamesField/src/app/app.ts
+++ GamesField/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './shared/footer/footer';
+import { Navigation } from './shared/navigation/navigation';
+import { Home } from './features/home/home';
+import { Filter } from './features/filter/filter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation, Footer, Filter, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
