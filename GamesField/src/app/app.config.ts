e19c619477440a453d3143131e3108b6de95c367
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-17 20:31:09 +0300

    no hashtags

--- GamesField/src/app/app.config.ts
+++ GamesField/src/app/app.config.ts
@@ -7,7 +7,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideHttpClient()
   ]
 };
