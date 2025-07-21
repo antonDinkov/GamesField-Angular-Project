526aec9cf2a22cf9cd1008d924e420b70f691a98
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-21 12:28:09 +0300

    home page navigation

--- GamesField/src/app/app.routes.ts
+++ GamesField/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    
+    {
+        path: "catalog",
+        loadComponent: () => import("./features/catalog/catalog").then(c => c.Catalog)
+    }
 ];
