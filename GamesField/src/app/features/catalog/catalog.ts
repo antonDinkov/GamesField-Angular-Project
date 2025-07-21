939f13af59fb408167500cbc13d1a4cba10589c2
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-21 14:14:06 +0300

    importing home-item component

--- GamesField/src/app/features/catalog/catalog.ts
+++ GamesField/src/app/features/catalog/catalog.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../../models/game.model';
 import { GamesSelection } from '../../core/services/games-selection';
+import { HomeItem } from '../home/home-item/home-item';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  imports: [HomeItem],
   templateUrl: './catalog.html',
   styleUrl: './catalog.css'
 })
