f5567d36c410b7bd070029bd944db91df52f86a4
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-22 15:52:47 +0300

    details template & css almost done

--- GamesField/src/app/app.routes.ts
+++ GamesField/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import("./features/home/home-board/home-board").then(c => c.HomeBoard)
+        loadComponent: () => import("./features/home/home-board/home-board").then(c => c.HomeBoard),
+        pathMatch: 'full'
     },
     {
         path: "catalog",
         loadComponent: () => import("./features/catalog/catalog").then(c => c.Catalog)
+    },
+    {
+        path: "details/:name/:id",
+        loadComponent: ()=> import("./features/details/details").then(c => c.Details)
     }
 ];
--- GamesField/src/app/features/home/home-item/home-item.ts
+++ GamesField/src/app/features/home/home-item/home-item.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Game } from '../../../models/game.model';
 import { PlayBtn } from '../../../shared/common/play-btn/play-btn';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-item',
   standalone: true,
-  imports: [PlayBtn],
+  imports: [PlayBtn, RouterModule],
   templateUrl: './home-item.html',
   styleUrls: ['./home-item.css']
 })
