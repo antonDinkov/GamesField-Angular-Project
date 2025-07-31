fbb31003e87e08b7329c3c76292b7a40c10acd88
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-31 10:16:55 +0300

    register, login, logout services

--- GamesField/src/app/core/services/auth-service.ts
+++ GamesField/src/app/core/services/auth-service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { User } from '../../models/user.model';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -13,14 +13,40 @@ export class AuthService {
 
     private _user = signal<User | null>(null);
     readonly user = this._user.asReadonly();
-    private apiUrl = environment.apiUrl
-    constructor(private httpClient: HttpClient) {}
-
-    register(firstName:string, lastName:string, email: string, password: string, repass: string): Observable<User> {
-        return this.httpClient.post<User>(`${this.apiUrl}/register`, {firstName, lastName, email, password, repass})
+    private apiUrl = environment.apiUrl;
+    constructor(private httpClient: HttpClient) {
+        const savedUser = localStorage.getItem('user');
+        if (savedUser) {
+            this._user.set(JSON.parse(savedUser));
+            this._isLoggedIn.set(true);
+        }
     }
 
-    login(email: string, password: string){
-        this.httpClient.post<User>(`${this.apiUrl}/login`, {email, password})
+    register(firstName: string, lastName: string, email: string, password: string, repass: string): Observable<User> {
+        return this.httpClient.post<User>(`${this.apiUrl}/register`, { firstName, lastName, email, password, repass }, { withCredentials: true })
+            .pipe(
+                tap((user) => {
+                    this._isLoggedIn.set(true);
+                    this._user.set(user);
+                    localStorage.setItem('user', JSON.stringify(user));
+                })
+            )
+    }
+
+    login(email: string, password: string): Observable<User> {
+        return this.httpClient.post<User>(`${this.apiUrl}/login`, { email, password }, { withCredentials: true })
+            .pipe(
+                tap((user) => {
+                    this._isLoggedIn.set(true);
+                    this._user.set(user);
+                    localStorage.setItem('user', JSON.stringify(user));
+                })
+            )
+    }
+
+    logout() {
+        this._user.set(null);
+        this._isLoggedIn.set(false);
+        localStorage.removeItem('user');
     }
 }
