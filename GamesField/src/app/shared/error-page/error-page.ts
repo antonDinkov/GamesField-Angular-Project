08ba37342378f50990f20b98ec4efcf926f1fb32
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-08-11 16:24:12 +0300

    error page animation

--- GamesField/src/app/shared/error-page/error-page.ts
+++ GamesField/src/app/shared/error-page/error-page.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
 } from '@angular/animations';
 @Component({
-  selector: 'app-error-page',
-  imports: [RouterLink],
-  templateUrl: './error-page.html',
-  styleUrl: './error-page.css',
-  animations: [
-    trigger('fadeInUp', [
-      state('void', style({ opacity: 0, transform: 'translateY(50px)' })),
-      transition(':enter', [
-        animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-      ])
-    ])
-  ]
+    selector: 'app-error-page',
+    imports: [RouterLink],
+    templateUrl: './error-page.html',
+    styleUrl: './error-page.css',
+    animations: [
+        trigger('fadeInUp', [
+            state('void', style({ opacity: 0, transform: 'translateY(50px)' })),
+            transition(':enter', [
+                animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+            ])
+        ])
+    ]
 })
 export class ErrorPage {
 
