f0ffb0c0a2fb54fad511ca4c8d399dec17cf1dbe
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-08-11 11:20:15 +0300

    auth guard & guest guard implementation

--- GamesField/src/app/app.routes.ts
+++ GamesField/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { authGuardGuard } from './core/guards/auth-guard-guard';
+import { guestGuardGuard } from './core/guards/guest-guard-guard';
 
 export const routes: Routes = [
     {
@@ -12,6 +14,7 @@ export const routes: Routes = [
     },
     {
         path: "create",
+        canActivate: [authGuardGuard],
         loadComponent: () => import("./features/create/create").then(c => c.Create)
     },
     {
@@ -20,30 +23,37 @@ export const routes: Routes = [
     },
     {
         path: "details/:name/:id/edit",
+        canActivate: [authGuardGuard],
         loadComponent: ()=> import("./features/details/edit/edit").then(c => c.Edit)
     },
     {
         path: "details/:name/:id/delete",
+        canActivate: [authGuardGuard],
         loadComponent: ()=> import("./features/details/delete/delete").then(c => c.Delete)
     },
     {
         path: "profile",
+        canActivate: [authGuardGuard],
         loadComponent: () => import("./features/profile/profile/profile").then(c => c.Profile)
     },
     {
         path: "profile/:id/edit",
+        canActivate: [authGuardGuard],
         loadComponent: () => import("./features/profile/edit/edit").then(c => c.Edit)
     },
     {
         path: "register",
+        canActivate: [guestGuardGuard],
         loadComponent: () => import("./features/auth/register/register").then(c => c.Register)
     },
     {
         path: "login",
+        canActivate: [guestGuardGuard],
         loadComponent: () => import("./features/auth/login/login").then(c => c.Login)
     },
     {
         path: "logout",
+        canActivate: [authGuardGuard],
         loadComponent: () => import("./features/auth/logout/logout").then(c => c.Logout)
     },
     {
